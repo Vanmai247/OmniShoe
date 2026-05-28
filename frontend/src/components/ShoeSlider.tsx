4efaad3f350fa125ac2ee7eb61da69aafcf49fb6
vanmh <vanmh@innotech-vn.com>
2026-05-28 15:35:28 +0700

    fix(slider): disable variant inheritance for float container to run animation on mount

--- frontend/src/components/ShoeSlider.tsx
+++ frontend/src/components/ShoeSlider.tsx
@@ -154,6 +154,7 @@ export default function ShoeSlider() {
           >
             {/* Sneaker floating container */}
             <motion.div
+              inherit={false}
               animate={{
                 y: [0, -16, 0],
               }}
